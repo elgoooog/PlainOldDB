87350a3bdaad76137d92e05cae39b69004591c24
Nicholas Hauschild <support@elgoooog.com>
2011-05-20 00:30:24 -0500

    Adding PropertyLoader.

---
+++ unit_tests/com/elgoooog/podb/util/PropertyLoaderTest.java
@@ -0,0 +1,15 @@
+package com.elgoooog.podb.util;
+
+import org.junit.Test;
+
+/**
+ * @author Nicholas Hauschild
+ *         Date: 5/20/11
+ *         Time: 12:23 AM
+ */
+public class PropertyLoaderTest {
+    @Test
+    public void propertyLoaderTest() throws Exception {
+        new PropertyLoader();
+    }
+}
